1ea302963d8144ae62071b232b669fbdb205e4cd
Junior Legal <jlegal@outlook.com>
2021-02-21 01:47:40 -0300

    se agrega modulo de administracio/crear-roles

---
+++ src/app/administracion/administracion.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CrearRolesComponent } from './pages/crear-roles/crear-roles.component';
+
+
+
+@NgModule({
+  declarations: [CrearRolesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdministracionModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearRolesComponent } from './administracion/pages/crear-roles/crear-roles.component';
 //Componentes
 import { LoginComponent } from './auth/pages/login/login.component';
 import { HomeComponent } from './home/pages/home/home.component';
@@ -9,6 +10,7 @@ import { AuthGuard } from './shared/guards/auth.guard';
 const routes: Routes = [
   { path:'login', component:LoginComponent },
   { path: 'home', component: HomeComponent, canActivate:[AuthGuard] },
+  {path:'administracion/crear-roles', component:CrearRolesComponent, canActivate:[AuthGuard]},
   {path:'', pathMatch:'full', redirectTo:'home'},
   { path:'**', pathMatch:'full', redirectTo:'home' }
 ];
